f60e577e41ff0d37ea296d2f82bb1f7f0484a81d
Oğulcan Önder <canonder9246@gmail.com>
2026-05-13 17:09:20 +0300

    remove default spring boot context load test

--- src/test/java/com/ogulcanonder/investment_tracking_app/InvestmentTrackingAppApplicationTests.java
+++ src/test/java/com/ogulcanonder/investment_tracking_app/InvestmentTrackingAppApplicationTests.java
@@ -5,11 +5,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class InvestmentTrackingAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }

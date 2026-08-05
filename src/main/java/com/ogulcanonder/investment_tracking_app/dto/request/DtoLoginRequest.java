a0ecccdab7d1d084d43a8cec2c5cb1971fbb0bb8
Oğulcan Önder <canonder9246@gmail.com>
2026-08-05 04:46:49 +0300

    Add login dto validation

--- src/main/java/com/ogulcanonder/investment_tracking_app/dto/request/DtoLoginRequest.java
+++ src/main/java/com/ogulcanonder/investment_tracking_app/dto/request/DtoLoginRequest.java
@@ -1,7 +1,13 @@
 package com.ogulcanonder.investment_tracking_app.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record DtoLoginRequest(
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Enter a valid email address")
         String email,
+        @NotBlank(message = "Password cannot be blank")
         String password
 ) {
 }
